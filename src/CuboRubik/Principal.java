db404e4287299707e75f75e4834892c076824736
JulianDavidAlarcon <julian.alarconp@cun.edu.co>
2024-08-25 09:02:51 -0500

    25/08/2024_Version1

---
+++ src/CuboRubik/Principal.java
@@ -0,0 +1,20 @@
+package CuboRubik;
+
+/**
+ * 20242008_Inteligencia Artificial_Cun
+ * Cubo Rubik_ V1
+ * @author Julián Alarcón - Albert Olarte - Sebastian Palacio
+ */
+public class Principal {
+
+    private void ejecutar() {
+        // Crear la interfaz gráfica del cubo Rubik
+        InterfazGrafica interfaz = new InterfazGrafica();
+        interfaz.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Principal().ejecutar();
+        // Instanciar y ejecutar la aplicación para visualizar el cubo Rubik
+    }
+}
